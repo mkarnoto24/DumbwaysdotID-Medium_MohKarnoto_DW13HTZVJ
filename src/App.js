b054bf49214c4226036e111f1c89c1b28aa59b71
noto24 <mohamadkarnoto90@gmail.com>
2019-12-07 21:55:08 +0700

    add src/component(Modal.js,bg.jpeg,style.css), update App.css,App.js,index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
+import Modal from './component/Modal';
 
-function App() {
+
+
+// end variabel
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +25,58 @@ function App() {
       </header>
     </div>
   );
+}*/
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="App">
+        <Header />
+        <Content />
+      </div>
+    );
+    /*return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">
+            Welcome to React
+          </h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload
+        </p>
+      </div>
+    );
+  }*/
+  }
 }
+class Header extends Component {
+  render() {
+
+    return (
+      <div>
+        <h1>This is Header</h1>
+      </div>
+    );
+  }
+}
+class Content extends Component {
+  render() {
+    return (
+      <div className="App-content">
+        <p>Welcome To DumbWays.id</p>
+        <Modal>
+        </Modal>
+
+      </div>
+
+    );
+  }
+}
+
+
+
 
 export default App;
